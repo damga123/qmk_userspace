2280926ec6da6073452994a0379f63bba55cb098
damga123 <damian.gaethofs@live.com>
2025-04-18 00:34:59 +0200

    Change attenuation

--- keyboards/splitkb/halcyon/kyria/keymaps/damga123_hlc/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/damga123_hlc/rules.mk
@@ -3,4 +3,4 @@ ENCODER_MAP_ENABLE = yes
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
 
-CIRQUE_PINNACLE_ATTENUATION = EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_2X
+CIRQUE_PINNACLE_ATTENUATION = EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_4X
