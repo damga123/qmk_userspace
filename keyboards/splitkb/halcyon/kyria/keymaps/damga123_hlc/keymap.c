de530d2624917193bc8db7582c45bb8637a8a214
damga123 <damian.gaethofs@live.com>
2025-04-18 13:01:59 +0200

    Add rgb control keys

--- keyboards/splitkb/halcyon/kyria/keymaps/damga123_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/damga123_hlc/keymap.c
@@ -127,8 +127,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FUNCTION] = LAYOUT_split_3x6_5_hlc(
   _______ , QWERTY  , KC_F9 , KC_F10  , KC_F11  , KC_F12  ,                                             _______ , _______ , _______ , _______ , _______ , _______,
   _______ , GAME    , KC_F5 , KC_F6   , KC_F7   , KC_F8   ,                                             _______ , KC_RSFT , KC_RCTL , KC_LALT , KC_RGUI , _______,
-  _______ , _______ , KC_F1 , KC_F2   , KC_F3   , KC_F4   , _______ , _______ ,     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______,
-                              _______ , _______ , KC_LALT , _______ , _______ ,     _______ , _______ , _______ , _______ , _______,
+  _______ , _______ , KC_F1 , KC_F2   , KC_F3   , KC_F4   , _______ , _______ ,     _______ , _______ , _______ , RM_TOGG , RM_NEXT , RM_PREV , _______ , _______,
+                              _______ , _______ , KC_LALT , _______ , _______ ,     _______ , RM_VALU , RM_VALD , RM_HUEU , RM_HUED ,
 
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     )
